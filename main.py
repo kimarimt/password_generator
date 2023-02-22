3e457c2e785113aa4155ed7ba66e052184ee2d07
Alec German <kimarimt@protonmail.com>
2023-02-21 19:29:45 -0500

    finished day 5 project

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+from random import choices, shuffle
+from string import ascii_letters, digits
+
+SYMBOLS = '!@#$%^&*()'
+
+
 def main():
-    print('Hello, World!')
+    print('Welcome to the PyPassword Generator')
+    letters = int(input('How many letters would you like in your password: '))
+    symbols = int(input('How many symbols would you like: '))
+    numbers = int(input('How many numbers would you like: '))
+
+    password = []
+    password.extend(choices(ascii_letters, k=letters))
+    password.extend(choices(SYMBOLS, k=symbols))
+    password.extend(choices(digits, k=numbers))
+    shuffle(password)
+
+    print(f'Here\'s your password: {"".join(password)}')
 
 
 if __name__ == '__main__':
